1d8f16d064da7c4fa3b8e7296eb7ab3b67bed258
Saurav Dhillon <souravdhillon010@gmail.com>
2025-01-22 01:19:17 -0500

    Implemented benchmark_with_block.rb benchmark method

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,9 @@
 def benchmark
   # Your benchmarking code goes here.
+  start_time = Time.now #Record the start time
+  yield  # Execute the block
+  end_time = Time.now # Record the end time
+  end_time - start_time # Return the time difference
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
